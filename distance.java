b70f0fd6c5b2480dd65ec74a9e0af2041f686253
SonalRajan19 <68916159+SonalRajan19@users.noreply.github.com>
2020-07-28 22:29:22 +0530

    Add files via upload

---
+++ distance.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class distance {
+    public static void main(String[] arg){
+        int x1=1,x2=2,y1=3,y2=4;
+        double distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println(distance);
+    }
+    
+}
